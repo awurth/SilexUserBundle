a320dced54cae13250ec73866005b6b064e3257b
Alexis Wurth <alexis.wurth57@gmail.com>
2017-08-08 13:52:52 +0200

    Added CSRF protection to login form

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,6 +3,8 @@
 namespace AWurth\SilexUser\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 /**
  * Silex User Authentication Controller.
@@ -13,9 +15,15 @@ class AuthController extends Controller
 {
     public function loginAction(Request $request)
     {
+        $csrfToken = null;
+        if (isset($this->application['csrf.token_manager']) && $this->application['csrf.token_manager'] instanceof CsrfTokenManagerInterface) {
+            $csrfToken = $this->application['csrf.token_manager']->getToken('authenticate');
+        }
+
         return $this->render('silex_user/auth/login.twig', [
             'error'         => $this->application['security.last_error']($request),
-            'last_username' => $this->application['session']->get('_security.last_username')
+            'last_username' => $this->application['session']->get(Security::LAST_USERNAME),
+            'csrf_token' => $csrfToken
         ]);
     }
 }
