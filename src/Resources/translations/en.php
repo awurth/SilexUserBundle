5c81c0a561023f60869827e33e671d52b8bfd9cc
Alexis Wurth <alexis.wurth57@gmail.com>
2017-08-19 18:47:03 +0200

    Added translations

--- src/Resources/translations/en.php
+++ src/Resources/translations/en.php
@@ -2,25 +2,41 @@
 
 return [
     'silex_user' => [
+        'login' => [
+            'title' => 'Login'
+        ],
+        'registration' => [
+            'title' => 'Register',
+            'check_email' => 'An email has been sent to %email%. It contains an activation link you must click to activate your account.',
+            'email' => [
+                'subject' => 'Welcome %username%!',
+                'message' => '
+                    Hello %username%!
+                    
+                    To finish activating your account - please visit %confirmationUrl%
+                    
+                    This link can only be used once to validate your account.
+                    
+                    Regards,
+                    the Team.'
+            ],
+            'confirmed' => [
+                'message' => 'Your account has been created successfully.',
+                'title' => 'Registration confirmed'
+            ],
+            'flash' => [
+                'user_created' => 'The user has been created successfully.'
+            ]
+        ],
         'form' => [
             'username' => 'Username',
             'password' => 'Password',
             'password_confirmation' => 'Repeat password'
         ],
-        'login' => [
-            'title' => 'Login'
-        ],
         'global' => [
             'login' => 'Log in',
             'logout' => 'Log out',
             'register' => 'Register'
-        ],
-        'registration' => [
-            'confirmed' => [
-                'message' => 'Your account has been created successfully.',
-                'title' => 'Registration confirmed'
-            ],
-            'title' => 'Register'
         ]
     ]
 ];
--- src/Resources/translations/fr.php
+++ src/Resources/translations/fr.php
@@ -2,25 +2,41 @@
 
 return [
     'silex_user' => [
+        'login' => [
+            'title' => 'Login'
+        ],
+        'registration' => [
+            'title' => 'Inscription',
+            'check_email' => 'Un e-mail a été envoyé à l\'adresse %email%. Il contient un lien d\'activation sur lequel il vous faudra cliquer afin d\'activer votre compte.',
+            'email' => [
+                'subject' => 'Welcome %username%!',
+                'message' => '
+                    Hello %username%!
+                    
+                    To finish activating your account - please visit %confirmationUrl%
+                    
+                    This link can only be used once to validate your account.
+                    
+                    Regards,
+                    the Team.'
+            ],
+            'confirmed' => [
+                'message' => 'Votre compte a été créé avec succès.',
+                'title' => 'Inscription terminée'
+            ],
+            'flash' => [
+                'user_created' => 'L\'utilisateur a été créé avec succès.'
+            ]
+        ],
         'form' => [
             'username' => 'Nom d\'utilisateur',
             'password' => 'Mot de passe',
             'password_confirmation' => 'Répéter le mot de passe'
         ],
-        'login' => [
-            'title' => 'Login'
-        ],
         'global' => [
             'login' => 'Connexion',
             'logout' => 'Déconnexion',
             'register' => 'Inscription'
-        ],
-        'registration' => [
-            'confirmed' => [
-                'message' => 'Votre compte a été créé avec succès.',
-                'title' => 'Inscription terminée'
-            ],
-            'title' => 'Inscription'
         ]
     ]
 ];
