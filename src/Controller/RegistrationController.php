abdb7ff49053de6a82bf87ea0a791d4aaf88be2f
Alexis Wurth <alexis.wurth57@gmail.com>
2017-08-19 18:39:39 +0200

    Added email confirmation actions

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,15 +2,15 @@
 
 namespace AWurth\SilexUser\Controller;
 
-use AWurth\SilexUser\Entity\UserManager;
+use AWurth\SilexUser\Entity\UserInterface;
 use AWurth\SilexUser\Event\FilterUserResponseEvent;
 use AWurth\SilexUser\Event\FormEvent;
 use AWurth\SilexUser\Event\GetResponseUserEvent;
 use AWurth\SilexUser\Event\Events;
 use AWurth\SilexUser\Form\RegistrationFormType;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Silex User Registration Controller.
@@ -21,10 +21,8 @@ class RegistrationController extends Controller
 {
     public function registerAction(Request $request)
     {
-        /** @var UserManager $userManager */
-        $userManager = $this->application['silex_user.user_manager'];
-        /** @var EventDispatcherInterface $dispatcher */
-        $dispatcher = $this->application['dispatcher'];
+        $userManager = $this->getUserManager();
+        $dispatcher = $this->getDispatcher();
 
         $user = $userManager->createUser();
         $user->setEnabled(true);
@@ -36,8 +34,7 @@ class RegistrationController extends Controller
             return $event->getResponse();
         }
 
-        /** @var FormInterface $form */
-        $form = $this->application['form.factory']->create(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationFormType::class, $user);
 
         $form->handleRequest($request);
 
@@ -49,7 +46,6 @@ class RegistrationController extends Controller
                 $userManager->updateUser($user);
 
                 $response = $event->getResponse();
-
                 if (null === $response) {
                     $response = $this->redirect('silex_user.registration_confirmed');
                 }
@@ -63,7 +59,6 @@ class RegistrationController extends Controller
             $dispatcher->dispatch(Events::REGISTRATION_FAILURE, $event);
 
             $response = $event->getResponse();
-
             if (null !== $response) {
                 return $response;
             }
@@ -74,8 +69,64 @@ class RegistrationController extends Controller
         ]);
     }
 
+    public function checkEmailAction()
+    {
+        $session = $this->getSession();
+        $email = $session->get('silex_user_confirmation_email');
+
+        if (empty($email)) {
+            return $this->redirect('silex_user.register');
+        }
+
+        $session->remove('silex_user_confirmation_email');
+        $user = $this->getUserManager()->findUserByEmail($email);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with email "%s" does not exist', $email));
+        }
+        
+        return $this->render('silex_user/registration/check_email.twig', [
+            'user' => $user
+        ]);
+    }
+
+    public function confirmAction(Request $request, $token)
+    {
+        $userManager = $this->getUserManager();
+
+        $user = $userManager->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with confirmation token "%s" does not exist', $token));
+        }
+
+        $dispatcher = $this->getDispatcher();
+
+        $user->setConfirmationToken(null);
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(Events::REGISTRATION_CONFIRM, $event);
+
+        $userManager->updateUser($user);
+
+        $response = $event->getResponse();
+        if (null === $response) {
+            $response = $this->redirect('silex_user.registration_confirmed');
+        }
+        
+        $dispatcher->dispatch(Events::REGISTRATION_CONFIRMED, new FilterUserResponseEvent($user, $request, $response));
+
+        return $response;
+    }
+
     public function confirmedAction()
     {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
         return $this->render('silex_user/registration/confirmed.twig');
     }
 }
