ff24a713282dacc0c135daf5d5f45df1adafe21d
Alexis Wurth <alexis.wurth57@gmail.com>
2017-08-20 20:20:48 +0200

    Fixed wrong interface

--- src/Event/FilterUserResponseEvent.php
+++ src/Event/FilterUserResponseEvent.php
@@ -4,7 +4,7 @@ namespace AWurth\SilexUser\Event;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\User\UserInterface;
+use AWurth\SilexUser\Entity\UserInterface;
 
 class FilterUserResponseEvent extends UserEvent
 {
