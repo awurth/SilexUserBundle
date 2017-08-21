b1ba464cb689d28a5555e0e04c0fd0fa65072496
Alexis Wurth <alexis.wurth57@gmail.com>
2017-08-21 12:04:17 +0200

    Added login_check route

--- src/Provider/SilexUserServiceProvider.php
+++ src/Provider/SilexUserServiceProvider.php
@@ -137,7 +137,13 @@ class SilexUserServiceProvider implements ServiceProviderInterface, BootableProv
         $controllers->get('/login', 'auth.controller:loginAction')
             ->bind('silex_user.login');
 
-        $controllers->get('/logout')->bind('silex_user.logout');
+        $controllers->method('GET|POST')
+            ->match('/login_check')
+            ->bind('silex_user.login_check');
+
+        $controllers->method('GET|POST')
+            ->match('/logout')
+            ->bind('silex_user.logout');
 
         $controllers->method('GET|POST')
             ->match('/register', 'registration.controller:registerAction')
